41b103975b1d20409da577bc662b789dd9573e07
zhangzeyang <zhangzeyang@huidr.com>
2021-04-26 18:41:20 +0800

    banner显示图片

--- test1launchmode/src/main/java/com/example/test1launchmode/MainActivity.kt
+++ test1launchmode/src/main/java/com/example/test1launchmode/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.test1launchmode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //
+        tv1.setOnClickListener{
+
+        }
+
+        tv2.setOnClickListener {
+
+        }
+
+        tv3.setOnClickListener {
+
+        }
+
+        tv4.setOnClickListener {
+
+        }
     }
 }
