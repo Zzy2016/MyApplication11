a5202575295501d383b69687f69484602d2fc681
zhangzeyang <zhangzeyang@huidr.com>
2021-06-08 18:26:56 +0800

    添加说明

--- AboutRecyclerView/src/main/java/com/example/myapplication3/MainActivity.kt
+++ AboutRecyclerView/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -35,6 +35,14 @@ class FlowerAdapter(var list: Array<String>) : RecyclerView.Adapter<FlowerAdapte
         return list.size
     }
 
+
+    /*
+    * ViewHolder 负责存储RecyclerView中每一个单独的表项所需显式的信息。
+    * RecyclerView只需要创建当前所显示的表项数量的ViewHolder外加存储中的几个ViewHolder即可
+    * 随着用户的滑动屏幕，ViewHolder会被回收，（）使用新数据填充），已有的表项会在一端小时，并且在另一端显示一个新的表项
+    * adapter从数据源获取数据，并将数据传递给正在更新其所持有试图的ViewHolder
+    *
+    * */
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //        constructor(itemView: View) : super(itemView)
         val tv: TextView = itemView.findViewById(R.id.tvItem)
